ce6658d9877ef4d4acacc981adfd65750304b063
SIRREG001 <udochukwureginald78@gmail.com>
2022-12-07 10:06:29 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const p1btn = document.querySelector("#p1btn");
-const p2btn = document.querySelector("#p2btn");
+const p1Btn = document.querySelector("#p1Btn");
+const p2Btn = document.querySelector("#p2Btn");
+const p1Display = document.querySelector("#p1Display")
+const p2Display = document.querySelector("#p2Display")
 
-p1btn.addEventListener("click", function(e){
-    alert('clicked')
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+
+p1Btn.addEventListener("click", function(e){
+    if (!isGameOver){
+        p1Score += 1;
+        if (p1Score === winningScore){
+          isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+p2Btn.addEventListener("click", function(e){
+    if (!isGameOver){
+        p2Score += 1;
+        if (p2Score === winningScore){
+          isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 })
